f2bdf57b3e6e76a0c6641bb2a861ae9006d1db88
tanurreh <tan.urrehm@gmail.com>
2023-09-16 15:22:49 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/cotton_plant/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cotton_plant
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomePageScreen extends StatelessWidget {
+  const HomePageScreen
+({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     body: Center(child: Text('hello')),
+    );
+  }
+}
